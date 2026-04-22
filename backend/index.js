b1e5e32d8a624270b4ea33d85005c1e8db9bdd88
just-tenacious <tshraddha1505@gmail.com>
2026-04-22 11:40:48 +0530

    base code

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,29 @@
 import express from "express";
-import mongoose from "mongoose";
+import mongoose, { get } from "mongoose";
 
 const app = express();
+
+const restaurant = new mongoose.Schema({
+    restaurant_name = String,
+    restaurant_location = String,
+    restaurant_owner_name = String,
+    restuarnt_owner_age = Number,
+    restaurant_doe = String
+});
+
+app.get("/", () => {
+    console.log("hello");
+})
+
+app.get("/get-data", () => {
+});
+
+app.post("/add");
+
+app.put("/edit/:id");
+
+app.delete("/delete/:id");
+
+app.listen(5000, () => {
+    console.log("server running on port 5000");
+})
